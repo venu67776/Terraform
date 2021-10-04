e341719648040908869dc0d0592ef35f2be84c50
venu67776 <83317608+venu67776@users.noreply.github.com>
2021-10-04 17:26:32 +0530

    new

---
+++ ec2/main.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+  profile = "default"
+  region  = "us-west-2"
+}
+
+resource "aws_instance" "venu" {
+  ami           = "ami-830c94e3"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "venu"
+  }
+}
+
