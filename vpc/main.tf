111292259009c337b3bf5988ae4831bb68e4a17e
venu67776 <83317608+venu67776@users.noreply.github.com>
2021-10-05 13:46:28 +0530

    new

--- vpc/main.tf
+++ vpc/main.tf
@@ -19,7 +19,7 @@ provider "aws" {
   region  = var.region
 }
 
-#create a vpc
+/*create vpc*/
 resource "aws_vpc" "vpc" {
   cidr_block              = var.vpc-cidr
   instance_tenancy        = "default"
@@ -30,6 +30,16 @@ resource "aws_vpc" "vpc" {
   }
 }
 
+/*creating internet_gateway*/
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.vpc.id
+
+  tags = {
+    Name = "IGW"
+  }
+}
+
+/*creating public subnets*/
 resource "aws_subnet" "public-subnet-1" {
   vpc_id                  = aws_vpc.vpc.id
   cidr_block              = var.public-subnet-1-cidr
@@ -63,6 +73,35 @@ resource "aws_subnet" "public-subnet-3" {
   }
 }
 
+/*creating public routetable*/
+resource "aws_route_table" "public-route-table" {
+  vpc_id       = aws_vpc.vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  tags       = {
+    Name     = "Public Route Table"
+  }
+}
+
+/*Associate Public Subnets to routetables*/
+resource "aws_route_table_association" "public-subnet-1-route-table-association" {
+  subnet_id           = aws_subnet.public-subnet-1.id
+  route_table_id      = aws_route_table.public-route-table.id
+}
+resource "aws_route_table_association" "public-subnet-3-route-table-association" {
+  subnet_id           = aws_subnet.public-subnet-2.id
+  route_table_id      = aws_route_table.public-route-table.id
+}
+resource "aws_route_table_association" "public-subnet-2-route-table-association" {
+  subnet_id           = aws_subnet.public-subnet-3.id
+  route_table_id      = aws_route_table.public-route-table.id
+}
+
+/*creating private subnets*/
 resource "aws_subnet" "private-subnet-1" {
   vpc_id                  = aws_vpc.vpc.id
   cidr_block              = var.private-subnet-1-cidr
