478ea6e937100189c4db9ef126fc82434c141bad
venu67776 <83317608+venu67776@users.noreply.github.com>
2021-10-05 12:12:53 +0530

    new

--- vpc/main.tf
+++ vpc/main.tf
@@ -1,9 +1,9 @@
-/*terraform {
-  backend "remote" {
+terraform {
+  /*backend "remote" {
   organization = "venuzs"
   workspaces {
      name = "venu"
-   }
+   }*/
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -12,16 +12,16 @@
   }
 
   required_version = ">= 0.14.9"
-}*/
+}
 
 provider "aws" {
   profile = "default"
-  region  = "${var.region}"
+  region  = var.region
 }
 
 #create a vpc
 resource "aws_vpc" "vpc" {
-  cidr_block              = "${var.vpc-cidr}"
+  cidr_block              = var.vpc-cidr
   instance_tenancy        = "default"
   enable_dns_hostnames    = true
 
