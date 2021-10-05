93a63a689dc302c0448154f762b5c6acf3030680
venu67776 <83317608+venu67776@users.noreply.github.com>
2021-10-05 14:00:44 +0530

    new

--- vpc/main.tf
+++ vpc/main.tf
@@ -1,9 +1,9 @@
 terraform {
-  backend "remote" {
-  organization = "venuzs"
-  workspaces {
-     name = "venu"
-   }
+  // backend "remote" {
+  // organization = "venuzs"
+  // workspaces {
+  //    name = "venu"
+  //  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
