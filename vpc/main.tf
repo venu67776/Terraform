af1bf5ff48bbd428d709795f65796f029ce5c520
venu67776 <83317608+venu67776@users.noreply.github.com>
2021-10-05 18:25:31 +0530

    new

--- vpc/main.tf
+++ vpc/main.tf
@@ -75,7 +75,6 @@ resource "aws_subnet" "public-subnet-3" {
 /*creating public routetable*/
 resource "aws_route_table" "public-route-table" {
   vpc_id       = aws_vpc.vpc.id
-
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.gw.id
