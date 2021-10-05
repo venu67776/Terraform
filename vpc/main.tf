7a131da138d0dee2e837df0bd1c7f66313bc09d2
venu67776 <83317608+venu67776@users.noreply.github.com>
2021-10-05 18:18:44 +0530

    new

--- vpc/main.tf
+++ vpc/main.tf
@@ -130,3 +130,44 @@ resource "aws_subnet" "private-subnet-3" {
     Name    = "private Subnet 3"
   }
 }
+
+/*terraform aws allocate elastic ip*/
+resource "aws_eip" "eip-for-nat-gateway-1" {
+  vpc    = true
+
+  tags   = {
+    Name = "EIP-1"
+  }
+}
+
+
+/*Create Nat Gateway  in Public Subnets*/
+
+resource "aws_nat_gateway" "nat-gateway-1" {
+  allocation_id = aws_eip.eip-for-nat-gateway-1.id
+  subnet_id     = aws_subnet.public-subnet-1.id
+
+  tags   = {
+    Name = "Nat Gateway Public Subnet 1"
+  }
+}
+
+/*Create Private Route Tables and Add Route Through Nat Gateway */
+
+resource "aws_route_table" "private-route-table-1" {
+  vpc_id            = aws_vpc.vpc.id
+
+  route {
+    cidr_block      = "0.0.0.0/0"
+    nat_gateway_id  = aws_nat_gateway.nat-gateway-1.id
+  }
+
+  tags   = {
+    Name = "Private Route Table 1"
+  }
+}
+
+resource "aws_route_table_association" "private-subnet-1-route-table-association" {
+  subnet_id         = aws_subnet.private-subnet-1.id
+  route_table_id    = aws_route_table.private-route-table-1.id
+}
