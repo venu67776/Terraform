8cd2dbb0596e7311f635d6f606400a206041c005
venu67776 <83317608+venu67776@users.noreply.github.com>
2021-10-05 15:00:39 +0530

    new

--- vpc/main.tf
+++ vpc/main.tf
@@ -46,8 +46,7 @@ resource "aws_subnet" "public-subnet-1" {
   vpc_id                  = aws_vpc.vpc.id
   cidr_block              = var.public-subnet-1-cidr
   availability_zone       = "us-east-1a"
-  map_public_ip_on_launch = true
-
+  
   tags      = {
     Name    = "Public Subnet 1"
   }
@@ -57,8 +56,7 @@ resource "aws_subnet" "public-subnet-2" {
   vpc_id                  = aws_vpc.vpc.id
   cidr_block              = var.public-subnet-2-cidr
   availability_zone       = "us-east-1b"
-  map_public_ip_on_launch = true
-
+  
   tags      = {
     Name    = "Public Subnet 2"
   }
@@ -68,7 +66,6 @@ resource "aws_subnet" "public-subnet-3" {
   vpc_id                  = aws_vpc.vpc.id
   cidr_block              = var.public-subnet-3-cidr
   availability_zone       = "us-east-1c"
-  map_public_ip_on_launch = true
 
   tags      = {
     Name    = "Public Subnet 3"
@@ -108,8 +105,7 @@ resource "aws_subnet" "private-subnet-1" {
   vpc_id                  = aws_vpc.vpc.id
   cidr_block              = var.private-subnet-1-cidr
   availability_zone       = "us-east-1a"
-  map_public_ip_on_launch = true
-
+ 
   tags      = {
     Name    = "private Subnet 1"
   }
@@ -119,8 +115,7 @@ resource "aws_subnet" "private-subnet-2" {
   vpc_id                  = aws_vpc.vpc.id
   cidr_block              = var.private-subnet-2-cidr
   availability_zone       = "us-east-1b"
-  map_public_ip_on_launch = true
-
+  
   tags      = {
     Name    = "private Subnet 2"
   }
@@ -130,8 +125,7 @@ resource "aws_subnet" "private-subnet-3" {
   vpc_id                  = aws_vpc.vpc.id
   cidr_block              = var.private-subnet-3-cidr
   availability_zone       = "us-east-1c"
-  map_public_ip_on_launch = true
-
+ 
   tags      = {
     Name    = "private Subnet 3"
   }
