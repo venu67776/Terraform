0923a339fbba180a517adf1d3da1d56c84a16a2d
venu67776 <83317608+venu67776@users.noreply.github.com>
2021-10-05 10:33:26 +0530

    new

--- ec2/main.tf
+++ ec2/main.tf
@@ -1,4 +1,10 @@
 terraform {
+  backend "remote" {
+  organization = "venuzs"
+  workspaces {
+     name = "Example-Workspace"
+   }
+ }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
