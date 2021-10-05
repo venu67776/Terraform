84462e271807d854069c5bd090eab2bfd7355063
venu67776 <83317608+venu67776@users.noreply.github.com>
2021-10-05 17:59:03 +0530

    git pull

--- vpc/main.tf
+++ vpc/main.tf
@@ -130,3 +130,156 @@ resource "aws_subnet" "private-subnet-3" {
     Name    = "private Subnet 3"
   }
 }
+
+resource "aws_route_table" "private-route-table-1" {
+  vpc_id       = aws_vpc.vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  tags       = {
+    Name     = "Public Route Table"
+  }
+}
+
+
+/*terraform aws allocate elastic ip*/
+resource "aws_eip" "eip-for-nat-gateway-1" {
+  vpc    = true
+
+  tags   = {
+    Name = "EIP 1"
+  }
+}
+resource "aws_eip" "eip-for-nat-gateway-2" {
+  vpc    = true
+
+  tags   = {
+    Name = "EIP 2"
+  }
+}
+resource "aws_eip" "eip-for-nat-gateway-3" {
+  vpc    = true
+
+  tags   = {
+    Name = "EIP 3"
+  }
+}
+
+/*Create Nat Gateway  in Public Subnets*/
+
+resource "aws_nat_gateway" "nat-gateway-1" {
+  allocation_id = aws_eip.eip-for-nat-gateway-1.id
+  subnet_id     = aws_subnet.public-subnet-1.id
+
+  tags   = {
+    Name = "Nat Gateway Public Subnet 1"
+  }
+}
+
+resource "aws_nat_gateway" "nat-gateway-2" {
+  allocation_id = aws_eip.eip-for-nat-gateway-2.id
+  subnet_id     = aws_subnet.public-subnet-1.id
+
+  tags   = {
+    Name = "Nat Gateway Public Subnet 2"
+  }
+}
+
+resource "aws_nat_gateway" "nat-gateway-3" {
+  allocation_id = aws_eip.eip-for-nat-gateway-3.id
+  subnet_id     = aws_subnet.public-subnet-1.id
+
+  tags   = {
+    Name = "Nat Gateway Public Subnet 3"
+  }
+}
+
+/*Create Private Route Tables and Add Route Through Nat Gateway */
+
+resource "aws_route_table" "private-route-table-1" {
+  vpc_id            = aws_vpc.vpc.id
+
+  route {
+    cidr_block      = "0.0.0.0/0"
+    nat_gateway_id  = aws_nat_gateway.nat-gateway-1.id
+  }
+
+  tags   = {
+    Name = "Private Route Table 1"
+  }
+}
+
+
+resource "aws_route_table" "private-route-table-2" {
+  vpc_id            = aws_vpc.vpc.id
+
+  route {
+    cidr_block      = "0.0.0.0/0"
+    nat_gateway_id  = aws_nat_gateway.nat-gateway-2.id
+  }
+
+  tags   = {
+    Name = "Private Route Table 2"
+  }
+}
+
+
+resource "aws_route_table" "private-route-table-1" {
+  vpc_id            = aws_vpc.vpc.id
+
+  route {
+    cidr_block      = "0.0.0.0/0"
+    nat_gateway_id  = aws_nat_gateway.nat-gateway-1.id
+  }
+
+  tags   = {
+    Name = "Private Route Table 1"
+  }
+}
+
+/*Associate Private Subnets with "Private Route Tables*/
+resource "aws_route_table_association" "private-subnet-1-route-table-association" {
+  subnet_id         = aws_subnet.private-subnet-1.id
+  route_table_id    = aws_route_table.private-route-table-2.id
+}
+
+resource "aws_route_table" "private-route-table-2" {
+  vpc_id            = aws_vpc.vpc.id
+
+  route {
+    cidr_block      = "0.0.0.0/0"
+    nat_gateway_id  = aws_nat_gateway.nat-gateway-2.id
+  }
+
+  tags   = {
+    Name = "Private Route Table 2"
+  }
+}
+
+
+resource "aws_route_table_association" "private-subnet-2-route-table-association" {
+  subnet_id         = aws_subnet.private-subnet-2.id
+  route_table_id    = aws_route_table.private-route-table-2.id
+}
+
+resource "aws_route_table" "private-route-table-3" {
+  vpc_id            = aws_vpc.vpc.id
+
+  route {
+    cidr_block      = "0.0.0.0/0"
+    nat_gateway_id  = aws_nat_gateway.nat-gateway-3.id
+  }
+
+  tags   = {
+    Name = "Private Route Table 3"
+  }
+}
+
+
+resource "aws_route_table_association" "private-subnet-3-route-table-association" {
+  subnet_id         = aws_subnet.private-subnet-3.id
+  route_table_id    = aws_route_table.private-route-table-3.id
+}
