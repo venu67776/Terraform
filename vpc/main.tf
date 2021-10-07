de484984015c51216b525387182662e0a1c66a89
venu67776 <83317608+venu67776@users.noreply.github.com>
2021-10-07 19:15:37 +0530

    new

--- vpc/main.tf
+++ vpc/main.tf
@@ -16,24 +16,24 @@
 #   required_version = ">= 0.14.9"
 # }
 
-#  terraform {
-#   backend "s3" {
-#     bucket = "venu6766"
-#     key    = "terraform.tfstate"
-#     region = "us-east-1"
-#     profile = "default"
-#     shared_credentials_file = "C:/Users/.aws/credentials"
-#   }
+ terraform {
+  backend "s3" {
+    bucket = "venu6766"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+    profile = "default"
+    shared_credentials_file = "C:/Users/.aws/credentials"
+  }
 
-#    required_providers {
-#     aws = {
-#        source  = "hashicorp/aws"
-#        version = "~> 3.27"
-#      }
-#    }
+   required_providers {
+    aws = {
+       source  = "hashicorp/aws"
+       version = "~> 3.27"
+     }
+   }
 
-#    required_version = ">= 0.14.9"
-#  }
+   required_version = ">= 0.14.9"
+ }
 
 provider "aws" {
   profile = "default"
